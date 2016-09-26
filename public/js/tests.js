f3f3a79c584b7cc88bfe779b78bd35e9f6fe64b8
tdeakin <amles.gamer@gmail.com>
2016-09-26 23:22:09 +1300

    Added more tests

--- public/js/tests.js
+++ public/js/tests.js
@@ -3,7 +3,6 @@ function ok(expr, msg) {
 }
 
 suite('Fleet');
-
 test('Purchase Small Ship', function () {
     resources.metal.amount = 500;
     fleet.methods.purchase(1);
@@ -30,13 +29,45 @@ test('Purchase Large Ship', function () {
 
 
 suite('Resource Storage');
-
 test('Hydrogen Storage', function () {
     ok(resources.hydrogen.maxStorage == 2000, "Initial storage should be 2000");
     resources.metal.amount = 100;
-    buildings.storage.metal.purchase();
+    buildings.storage.hydrogen.purchase();
 
     ok(resources.hydrogen.maxStorage == 2500, "Buying a hydrogen tank should increase the amount of storage by 500");
     ok(resources.metal.amount == 0, "Buying a hydrogen tank should cost 100 metal");
+
+});
+test('Food Storage', function () {
+    ok(resources.food.maxStorage == 2000, "Initial storage should be 2000");
+    resources.metal.amount = 100;
+    buildings.storage.food.purchase();
+
+    ok(resources.food.maxStorage == 2500, "Buying a hydrogen tank should increase the amount of storage by 500");
+    ok(resources.metal.amount == 0, "Buying a hydrogen tank should cost 100 metal");
+
+});
+test('Metal Storage', function () {
+    ok(resources.metal.maxStorage == 2000, "Initial storage should be 2000");
+    resources.metal.amount = 100;
+    buildings.storage.metal.purchase();
+
+    ok(resources.metal.maxStorage == 2500, "Buying a hydrogen tank should increase the amount of storage by 500");
+    ok(resources.metal.amount == 0, "Buying a hydrogen tank should cost 100 metal");
+
+});
+
+suite('Housing');
+test('Apartment', function () {
+
+    ok(population.currentPopulationCap == 30, "default pop cap should be 30");
+    buildings.housing.apartment.purchase();
+    ok(population.currentPopulationCap == 30, "pop cap should still be 30, as we can't afford a house");
+    resources.metal.amount = 50;
+    buildings.housing.apartment.purchase();
+    ok(population.currentPopulationCap == 32, "Now that we have an apartment, the cap should be 32");
+
+    ok(buildings.housing.apartment.metalCost == 55, "Metal cost should increase by 5");
+
 
 });
